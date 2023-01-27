787da4af31c5df0c21c26b1547d1003a56cdef16
Hitodama0 <biagior00@gmail.com>
2023-01-27 11:50:24 +0100

    Initial Commit

---
+++ TemperatureConverter/ContentView.swift
@@ -0,0 +1,77 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State  var inputNumber: Double = 0
+    @State  var inputUnit: String = "Celsius"
+    @State  var outputUnit: String = "Celsius"
+    @FocusState private var isFocused: Bool
+    
+    let units = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    
+    var input: Double {
+        let number = inputNumber
+        if inputUnit == "Fahrenheit" {
+            return ((inputNumber - 32) * 5/9)
+        }
+        else  if inputUnit == "Kelvin" {
+            return (inputNumber - 273.15)
+        }
+        else if inputUnit == "Celsius" {
+            return inputNumber
+        }
+        return number
+    }
+    
+    
+    var outputUnits: Double {
+        let number = input
+        if outputUnit == "Celsius" {
+            return number
+        }
+        else if outputUnit == "Fahrenheit" {
+            return ((number * (9/5)) + 32)
+        }
+        else if outputUnit == "Kelvin" {
+            return (number + 273.15)
+        }
+         return number
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            Form{
+                Section{
+                    TextField("Input value", value: $inputNumber, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    Picker("Input unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                Section{
+                    Text(outputUnits, format: .number)
+                } header: {
+                    Text("Your conversion is: ")
+                }
+            }
+            .navigationTitle("TemperatureConv")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Biagio Ricci on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
